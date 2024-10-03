05627e89caf76d4918acc5122ab220d28cd8f507
Filip Ebert <65273341+TemnyZlo@users.noreply.github.com>
2024-10-03 16:33:32 +0200

    1

---
+++ MVC - complete architecture/MVC - complete architecture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC___complete_architecture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
