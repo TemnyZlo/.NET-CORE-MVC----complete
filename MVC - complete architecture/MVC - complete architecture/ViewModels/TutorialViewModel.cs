3bf965e63d347b25b1cab6087a20fcf6de8144da
Filip Ebert <65273341+TemnyZlo@users.noreply.github.com>
2024-12-05 15:56:40 +0100

    end

--- MVC - complete architecture/MVC - complete architecture/ViewModels/TutorialViewModel.cs
+++ MVC - complete architecture/MVC - complete architecture/ViewModels/TutorialViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using MVC___complete_architecture.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVC___complete_architecture.ViewModels
 {
     public class TutorialViewModel
     {
-        public Tutorial tutorial { get; set; }
+        //public Tutorial tutorial { get; set; }
+        public int Id { get; set; }
 
-        public string title { get; set; }
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Please enter text")]
+        [Required]
+        [Display(Name = "Enter the name of tutorial")]
+        public string Name { get; set; }
 
-
+        [Required]
+        [Display(Name = "Please describe your tutorial")]
+        public string Description { get; set; }
     }
 }
